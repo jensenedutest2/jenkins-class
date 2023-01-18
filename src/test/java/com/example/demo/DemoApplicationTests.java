4f21de4ebc4f28d2b9ebd57fb8100513fd4accdf
jensenedutest1 <jensenedutest1@gmail.com>
2023-01-18 09:16:35 +0100

    initial commit

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DemoApplicationTests
+{
+	@Test
+    public void contextLoads()
+    {
+		System.out.println("Great, context loaded.");
+    }
+	
+	@Test
+	public void testAddNumbers()
+	{
+		int res = DemoApplication.addNumbers(2, 5);
+		assertThat(res).isEqualTo(2+5);
+	}
+}
